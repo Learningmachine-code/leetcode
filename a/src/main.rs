9e1ae7bd80576c4ecc56a25f8639f51e9ab9b861
Learningmachine-code <learningmachines@outlook.com>
2024-10-15 10:58:58 +0800

    2024-10-15

--- a/src/main.rs
+++ a/src/main.rs
@@ -1,3 +1,7 @@
+use a::Solution;
 fn main() {
-    println!("Hello, world!");
+    let nums1=vec![1,2,3,4,5,6];
+    let nums2=vec![2,3,4,5,6,7];
+    let result=Solution::find_median_sorted_arrays(nums1,nums2);
+    println!("{result}");
 }
